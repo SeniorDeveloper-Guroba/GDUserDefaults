5cd5d25d5048b388d38d5dc03ba46f97c203d740
Aiub Duduev <aub.duduev.1989@gmail.com>
2022-11-28 12:23:47 +0300

    init

---
+++ Sources/GDUserDefaults/GDUserDefaults.swift
@@ -0,0 +1,39 @@
+
+import Foundation
+
+public final class UserDefaultsStandard {
+    
+    static let shared = UserDefaultsStandard()
+    
+    private let standard = UserDefaults.standard
+    
+    public func save<T>(key: StoreProjectKey.Key, value: T) {
+        standard.set(value, forKey: key.rawValue)
+    }
+    
+    public func get<T: Any>(key: StoreProjectKey.Key) -> T? {
+        return standard.object(forKey: key.rawValue) as? T
+    }
+    
+    public func save<T>(key: String, value: T) {
+        standard.set(value, forKey: key)
+    }
+    
+    public func get<T: Any>(key: String) -> T? {
+        return standard.object(forKey: key) as? T
+    }
+}
+
+public class StoreProjectKey {
+    
+    public enum Keys {
+        case nul
+    }
+    
+    public enum Key: String {
+        case nul
+        case token
+        case isRegistration
+        case userID
+    }
+}
