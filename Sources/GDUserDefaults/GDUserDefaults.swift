347e3f6304e7ad9736ff794842a839a9dd6b6188
Aiub Duduev <aub.duduev.1989@gmail.com>
2022-12-03 10:27:32 +0300

    fix

--- Sources/GDUserDefaults/GDUserDefaults.swift
+++ Sources/GDUserDefaults/GDUserDefaults.swift
@@ -7,19 +7,19 @@ public final class UserDefaultsStandard {
     
     private let standard = UserDefaults.standard
     
-    public func saveForKey<T>(key: StoreProjectKey.Key, value: T) {
+    public func save<T>(key: StoreProjectKey.Key, value: T) {
         standard.set(value, forKey: key.rawValue)
     }
     
-    public func getForKey<T: Any>(key: StoreProjectKey.Key) -> T? {
+    public func get<T: Any>(key: StoreProjectKey.Key) -> T? {
         return standard.object(forKey: key.rawValue) as? T
     }
     
-    public func saveForString<T>(key: String, value: T) {
+    public func save<T>(key: String, value: T) {
         standard.set(value, forKey: key)
     }
     
-    public func getForString<T: Any>(key: String) -> T? {
+    public func get<T: Any>(key: String) -> T? {
         return standard.object(forKey: key) as? T
     }
 }
@@ -37,5 +37,6 @@ public class StoreProjectKey {
         case userID
         case isMute
         case score
+        case isMusic
     }
 }
